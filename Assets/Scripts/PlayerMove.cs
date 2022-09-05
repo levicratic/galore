59313e546f8c5f1710f76383c18c329c27b22f06
J203g <61242472+J203g@users.noreply.github.com>
2022-09-05 18:32:02 -0400

    Game Jam

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
 
-    public 
+    public Rigidbody rb;
+    public float movementSpeed;
+
+    Vector3 mousePosition;
+    Vector3 relativeDirection;
+    Vector3 lookDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +23,33 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // get x, y coords of mouse
+        mousePosition = Mouse.current.position.ReadValue();
+
+        // convert to world point
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // direction relative to the ship object
+        relativeDirection = mousePosition - transform.position;
+
+        // rotation of the Z-axis, get angle with arctan(o/a)
+        float relZ = Mathf.Atan(relativeDirection.x / Mathf.Abs(relativeDirection.y)) / Mathf.PI * 360;
+        relZ = Mathf.Clamp(relZ*Mathf.Log10(relativeDirection.magnitude/10), -90, 90);
+
+        transform.LookAt(mousePosition);
+        transform.Rotate(0, 0, relZ);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce((relativeDirection + relativeDirection.normalized * 10f) * movementSpeed, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        SceneManager.LoadScene(0);
         
     }
 }

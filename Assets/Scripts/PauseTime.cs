dbacdeeacea076d3a46db46f4328c721a503a512
J203g <61242472+J203g@users.noreply.github.com>
2022-09-10 14:07:50 -0400

    .gitignore update

--- Assets/Scripts/PauseTime.cs
+++ Assets/Scripts/PauseTime.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 /* -- FEATURES --
+ * Mouse cursor is hidden during gameplay
  * Pause when ESC is pressed. Unpause when pressed again.
+ *  - pausing/unpausing must show and hide the mouse cursor, respectively
  * Rapidly slow time to half-speed when space bar is pressed
- * Gradually speed time back up after 1.5-2 seconds of slow
- * If space bar is released, then super rapidly bring it back to 1
- * Have a slightly cooldown before you can slow again
+ * Gradually speed time back up after 1.5ish seconds of slow (timeout condition)
+ * If space bar is released, then rapidly bring it back to 1 (user stopped condition)
+ * Have a slight cooldown before you can slow again -- not implemented, does it need it?
  */
 
 public class PauseTime : MonoBehaviour
@@ -25,16 +27,15 @@ public class PauseTime : MonoBehaviour
     [Header("Multipliers")]
     public float slowMult = 6f;
 
-    private float timeScale
+    private float timeScale // every time the time scale is changed
     {
         get { return Time.timeScale; }
         set {
             Time.timeScale = value > .999f ? 1 : value;
             Time.fixedDeltaTime = 0.02f * timeScale;
-
             lightDim.intensity = timeScale * timeScale;
             audioDim.pitch = timeScale;
-            _inverse = 1;
+            _inverse = 1; // apply change to all variables, then reset the inverse curve
         }
     }
 
@@ -61,10 +62,12 @@ public class PauseTime : MonoBehaviour
 
     private void Start()
     {
-        pause.started += Pause; // toggle
+        pause.started += TogglePause; // toggle
 
         slow.started += _ => slowing = true; //InvokeRepeating("Slow", 0, .02f);
         slow.canceled += _ => slowing = false;
+
+        Cursor.visible = false;
     }
 
     private void OnEnable()
@@ -85,41 +88,43 @@ public class PauseTime : MonoBehaviour
 
         if (slowing && timeScale > .5001f) // slow in, timeout at .5001
         {
+            timeScale = slowMult * timeScale / (slowMult - .5f + timeScale);
             timeout = false;
-            timeScale = slowMult * timeScale / (slowMult - .5f + timeScale); // fade to .5 fast
         }
         else if (timeScale < .999f)
         {
             if (slowing || timeout)
             {
+                timeScale = slowMult * 3 * timeScale / (slowMult * 3 - 1 + timeScale); // slowly return to normal
                 slowing = false; timeout = true;
-                timeScale = slowMult * 3 * timeScale / (slowMult * 3 - 1 + timeScale); // fade to 1 slower
             }
             else
             {
-                //timeScale = slowMult / 2 * timeScale / (slowMult / 2 - 1 + timeScale); // fade to 1 faster
-                inverseScale = slowMult * inverseScale / (slowMult - .5f + inverseScale); // fade to 1 inverse
+                //timeScale = slowMult / 2 * timeScale / (slowMult / 2 - 1 + timeScale); // rapidly return to 1
+                inverseScale = slowMult * inverseScale / (slowMult - .5f + inverseScale);
             }
         }
 
         // update variables to match scale
     }
 
-    private void Pause(InputAction.CallbackContext obj)
+    private void TogglePause(InputAction.CallbackContext obj)
     {
-        if (!paused)
+        if (!paused) // pause
         {
             timeScale = 0;
             spritePause.SetActive(true);
             audioSource.Pause();
             paused = true;
+            Cursor.visible = true;
         }
-        else
+        else // unpause
         {
             timeScale = 1;
             spritePause.SetActive(false);
             audioSource.UnPause();
             paused = false;
+            Cursor.visible = false;
         }
     }
 
